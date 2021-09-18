80e498025d0171a0c2e5fccab24ac3e098a5d673
Alexey Kuzniecov <alexeyk90@gmail.com>
2021-09-18 19:14:53 +0300

    feat: add final styles

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ burger.addEventListener('click', () => {
   headerMenu.classList.toggle('active');
   burger.classList.toggle('active');
 })
+
+console.log('%c> Burger menu on 900px width breakpoint', 'color: green')
