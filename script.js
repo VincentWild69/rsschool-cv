507ceed4e6887835cdee2551eeec4956c4e500c1
Alexey Kuzniecov <alexeyk90@gmail.com>
2021-12-15 13:23:08 +0200

    fix: fix menu behaviour

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const headerMenu = document.querySelector('.header__menu');
 burger.addEventListener('click', () => {
   headerMenu.classList.toggle('active');
   burger.classList.toggle('active');
-})
+
+  if (headerMenu.classList.contains('active')) {
+    setTimeout(() => {
+      document.addEventListener('click', foo = (e) => {
+      if (headerMenu.classList.contains('active') && !e.target.closest('.header__menu') && !e.target.closest('.burger')) {
+        headerMenu.classList.remove('active');
+        burger.classList.remove('active');
+        document.removeEventListener('click', foo)
+        }
+      })
+    }, 0)
+  } else {
+      document.removeEventListener('click', foo)
+    }
+
+})    
+
+
+
+
 
 console.log('%c> Burger menu on 900px width breakpoint', 'color: green')
